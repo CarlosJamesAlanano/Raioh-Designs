629b4a8e6d32dba891334cea9431a1a1dd7e8518
CarlosJamesAlanano <cjalanano.dev@gmail.com>
2025-03-21 14:27:52 +0800

    Add contact form section and improve navigation button styles; Penultimate commit

--- js/script.js
+++ js/script.js
@@ -4,3 +4,46 @@ burgir.addEventListener("click", () => {
     burgir.classList.toggle("open");
     hNav.classList.toggle("open");
 })
+
+const contactButton = document.querySelector(".contact_button");
+const contactForm = document.querySelector(".contact_forms");
+const backDrop = document.querySelector(".backdrop");
+
+contactButton.addEventListener("click", () => {
+    if(contactForm.classList.contains("open")){
+        contactForm.classList.remove("open");
+        contactButton.classList.remove("open");
+        backDrop.classList.remove("open");
+    } else{
+        contactForm.classList.add("open");
+        contactButton.classList.add("open");
+        backDrop.classList.add("open");
+    }
+})
+
+const home = document.querySelector(".btnHome");
+const abt = document.querySelector(".btnAbout");
+const evnts = document.querySelector(".btnEvents");
+const prgrms = document.querySelector(".btnPrograms");
+const news = document.querySelector(".btnNews");
+
+home.addEventListener("click", () => {
+    burgir.classList.remove("open");
+    hNav.classList.remove("open");
+});
+abt.addEventListener("click", () => {
+    burgir.classList.remove("open");
+    hNav.classList.remove("open");
+});
+evnts.addEventListener("click", () => {
+    burgir.classList.remove("open");
+    hNav.classList.remove("open");
+});
+prgrms.addEventListener("click", () => {
+    burgir.classList.remove("open");
+    hNav.classList.remove("open");
+});
+news.addEventListener("click", () => {
+    burgir.classList.remove("open");
+    hNav.classList.remove("open");
+});
